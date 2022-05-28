f00b01e981efc158ba799927b32247ecb95176a4
Денис <denisegorenko1@gmail.com>
2022-05-28 18:17:12 +0300

    First Task

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Message from './Message';
+import styles  from './Message.module.css'
 
 const messageData = {
     avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
@@ -12,20 +14,36 @@ function HW1() {
         <div>
             <hr/>
             homeworks 1
-
+            <hr/>
             {/*should work (должно работать)*/}
+            <div className={styles.messages}>
+                <Message
+                    avatar={messageData.avatar}
+                    name={messageData.name}
+                    message={messageData.message}
+                    time={messageData.time}
+                />
 
-            {/*<Message*/}
-            {/*    avatar={messageData.avatar}*/}
-            {/*    name={messageData.name}*/}
-            {/*    message={messageData.message}*/}
-            {/*    time={messageData.time}*/}
-            {/*/>*/}
+                <Message
+                    avatar={messageData.avatar}
+                    name={messageData.name}
+                    message={messageData.message}
+                    time={messageData.time}
+                />
+
+                <Message
+                    avatar={messageData.avatar}
+                    name={messageData.name}
+                    message={messageData.message}
+                    time={messageData.time}
+                />
+            </div>
+
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativeMessage/>*/}
-            <hr/>
+            {/*<hr/>*/}
         </div>
     )
 }
--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import styles from "./Message.module.css"
 
-function Message() {
+
+type MessagePropsType = {
+    avatar: string,
+    name: string,
+    message: string,
+    time: string
+}
+
+
+
+function Message(props: MessagePropsType) {
     return (
-        <div>
+        <div className={styles.messageBlock}>
+            <div className={styles.avatar}>
+                <img src={props.avatar}/>
+            </div>
+            {/*<div className={styles.message}>*/}
+            {/*    Message*/}
+            {/*</div>*/}
+
+
+
+            <div className={styles.message}>
+
+                <div className={styles.sideFigure}>
+                    <div className={styles.sideBottomPeace}>
+                        <div className={styles.sideCircle}></div>
+                        <div className={styles.sidePeace}></div>
+                    </div>
+                </div>
+
+                <div className={styles.messageBody}>
+                    <div className={styles.messageBodyName}>
+                        {props.name}
+                    </div>
+                    <div className={styles.messageBodyMessage}>
+                        {props.message}
+                    </div>
+                    <div className={styles.messageBodyTime}>
+                        {props.time}
+                    </div>
+                </div>
+            </div>
 
         </div>
     )
