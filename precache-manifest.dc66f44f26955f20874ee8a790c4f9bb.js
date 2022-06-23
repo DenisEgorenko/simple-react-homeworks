726b16aff71f9ca3a92aa0ea1259f991505f40c3
Денис <denisegorenko1@gmail.com>
2022-06-23 15:21:39 +0300

    Updates

---
+++ precache-manifest.dc66f44f26955f20874ee8a790c4f9bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22b1e3f83223870304a05ee71d7f8ea2",
+    "url": "/simple-react-homeworks/index.html"
+  },
+  {
+    "revision": "bfcbe1b29dd8a74af8f1",
+    "url": "/simple-react-homeworks/static/css/main.06d9dbd3.chunk.css"
+  },
+  {
+    "revision": "97153af6856542f23fd4",
+    "url": "/simple-react-homeworks/static/js/2.ea8509de.chunk.js"
+  },
+  {
+    "revision": "248fe9facfff898840212404b4cb88d9",
+    "url": "/simple-react-homeworks/static/js/2.ea8509de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfcbe1b29dd8a74af8f1",
+    "url": "/simple-react-homeworks/static/js/main.84f53cde.chunk.js"
+  },
+  {
+    "revision": "35a9557543d5cb14c0c5",
+    "url": "/simple-react-homeworks/static/js/runtime-main.5a451111.js"
+  }
+]);
